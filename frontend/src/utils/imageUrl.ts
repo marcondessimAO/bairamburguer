e6f69026ae0097dbf6542275808438e66b8788b4
Marcondes Simão <zonasulta@gmail.com>
2026-05-26 00:53:32 -0300

    front deploy

--- frontend/src/utils/imageUrl.ts
+++ frontend/src/utils/imageUrl.ts
@@ -1,7 +1,11 @@
 export function getImageUrl(url: string | undefined | null) {
   if (!url) return '/images/bairam-logo.jpg.jpg'; // Garante que nunca quebra se for null
   if (url.startsWith('http')) return url;
+  
+  
+ const host = process.env.NEXT_PUBLIC_HOST_URL || 'http://2.25.131.139:8080';
+  
   // Garante que a barra existe entre o host e o caminho
   const cleanUrl = url.startsWith('/') ? url : `/${url}`;
-  return `http://localhost:8080${cleanUrl}`;
+  return `${host}${cleanUrl}`;
 }
